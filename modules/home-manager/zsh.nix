f1712a2584e9c42adf87497896364e5b2e319be5
Gurvan Biguet--Kerloc'h <gurvan.biguet-kerloch@epita.fr>
2026-08-23 21:49:12 +0200

    fix(zsh): submit alias

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -75,7 +75,7 @@
         bindkey "^[[1;5C" forward-word
         bindkey "^[[1;5D" backward-word
 
-        submit () { git tag -ma "$1" && git push "$1" }
+        submit() {git tag -m "$1" "$1" && git push origin "$1"}
 
         export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 

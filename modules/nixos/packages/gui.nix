dba939ff2d8f453f05e2099e0c06f03e63e89c10
Gurvan Biguet--Kerloc'h <gurvan.biguet-kerloch@epita.fr>
2026-09-14 14:59:29 +0200

    feat: add trilium-desktop

--- modules/nixos/packages/gui.nix
+++ modules/nixos/packages/gui.nix
@@ -1,6 +1,3 @@
-_: {
-  # This file is now empty of direct package declarations
-  # as GUI apps have moved to Home Manager.
-  # It can be used for system-wide GUI-related configuration if needed.
-  environment.systemPackages = [ ];
+{ pkgs, ... }: {
+  environment.systemPackages = [ pkgs.trilium-desktop ];
 }
